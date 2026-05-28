801c918a838f83ce4e62886fe9a184998fb9f83e
WantechFan <alifiandratsaqifp@gmail.com>
2026-05-28 18:09:33 +0700

    Fahh

--- mod.js
+++
@@ -1,8 +0,0 @@
-const name = "overpowered-scathe";
-const displayName = "Overpowered Scathe";
-const author = "WantechFan";
-const description = "Buffs Scathe.";
-const minGameVersion = "155.2";
-const version = "1.0";
-const hidden = true;
-const subtitle = "Buffs Scathe";
