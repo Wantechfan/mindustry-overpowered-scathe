51e6f68ba526675cd865655e8dee12cb48206373
WantechFan <alifiandratsaqifp@gmail.com>
2026-05-28 17:59:29 +0700

    Heheheha

---
+++ mod.js
@@ -0,0 +1,8 @@
+const name = "wantech-test-mod";
+const displayName = "WantechFan's Testing Mod";
+const author = "WantechFan";
+const description = "A mod used for WantechFan's testings.";
+const minGameVersion = "155.2";
+const version = "1.5";
+const hidden = true;
+const subtitle = "Why would you download this?";
